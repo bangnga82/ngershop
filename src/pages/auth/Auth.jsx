911ebeff42365cf79e3157769720bb7a82fe1142
Bàng Nga <bangnga82@gmail.com>
2026-03-18 23:03:09 +0700

    update auth

---
+++ src/pages/auth/Auth.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import lineTop from "../../assets/images/lineTop.png";
+import lineLeft from "../../assets/images/lineLeft.png";
+import NoLoginForm from "@/components/auth/login/NoLoginForm";
+import "./Auth.scss";
+import NoRegister from "@/components/auth/register/NoRegister";
+import RegisterForm from "@/components/auth/register/RegisterForm";
+import LoginForm from "@/components/auth/login/LoginForm";
+const Auth = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  return (
+    <div
+      className="auth"
+      style={isLogin ? { height: "100vh" } : { height: "130vh" }}
+    >
+      <div
+        className="auth-container"
+        style={isLogin ? { height: "700px" } : { height: "900px" }}
+      >
+        <div className="auth-container_left">
+          <img className="line-top" src={lineTop} />
+          <img className="line-bottom" src={lineTop} />
+          <img className="line-left" src={lineLeft} />
+          <img className="line-right" src={lineLeft} />
+          {isLogin ? (
+            <NoLoginForm setIsLogin={setIsLogin} />
+          ) : (
+            <NoRegister setIsLogin={setIsLogin} />
+          )}
+        </div>
+        <div className="auth-container_right">
+          {isLogin ? (
+            <LoginForm setIsLogin={setIsLogin} />
+          ) : (
+            <RegisterForm setIsLogin={setIsLogin} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
